e1ff70498467fd60d3eca14a1f4b3be3b226f570
ehyewon <a01033171516@gmail.com>
2025-10-21 17:15:35 +0900

    레포 이름 지정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-static-page/'
+  base: 'https://github.com/ehyewon/vue-static-page.git'
 })
